3fd5793364a6c51a94ad6c7e3d65f7bcbeb4a6a2
KingOfPython <kimnayer@gmail.com>
2019-06-11 23:04:57 +0900

    mysql 에러 수정 반영 및 기타 소소한 에러 수정

--- nodejs/index.js
+++ nodejs/index.js
@@ -11,13 +11,36 @@ var connection = mysql.createConnection({
     password: '*****',
     database: 'project_db'
 })
-connection.connect();
 
+// connection.connect();
+// db 끊키는 문제 해결 (19.05.21:12:10 추가)
+// https://github.com/mysqljs/mysql/issues/431#issuecomment-15606772
+// https://berr.tistory.com/222
+function handleDisconnect(conn){
+    conn.on('error', function(err){
+        if (!err.fatal){
+            return;
+        }
+        if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+            throw err;
+        }
+        console.log('Re-connecting lost connection: ' + err.stack);
+        connection =  mysql.createConnection({
+            host: 'localhost',
+            user: '*****',
+            password: '*****',
+            database: 'project_db'
+        });             // 보안 고려 하여 코드 수정 필요
+        handleDisconnect(connection);
+        conn.connect();
+    })
+}
+handleDisconnect(connection);
 
 // localhost:8000/show
 app.get('/show', function (req, res) {
 
-    fs.readFile('../html/list.html', 'utf8', function (error, data) {
+    fs.readFile('../web/list.html', 'utf8', function (error, data) {
         if (error) {
             console.log('readFile error');
         } else {
